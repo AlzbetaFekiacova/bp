a56ca5629db4f8684585a8dd0bd33759f95b6fc4
Alžbeta Fekiačová <xfekiacova@stuba.sk>
2021-12-02 22:38:36 +0100

    Initial commit

---
+++ app/src/main/java/sk/mpage/myapplication/PermissionManager.kt
@@ -0,0 +1,12 @@
+package sk.mpage.myapplication
+
+
+import android.app.Activity
+import android.widget.Toast
+import com.mapbox.android.core.permissions.PermissionsListener
+import com.mapbox.android.core.permissions.PermissionsManager
+import java.lang.ref.WeakReference
+
+class PermissionManager {
+
+}
